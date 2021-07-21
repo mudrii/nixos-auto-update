06c875054d2a5e60b8cd5cc537417c50320df1d6
mudrii <mudreac@gmail.com>
2021-07-21 12:52:47 +0800

    changes for path for nixos-rebuild

--- modules/module.nix
+++ modules/module.nix
@@ -10,30 +10,19 @@ with lib;
       type = package;
       default = pkgs.git;
     };
-    #nixFlakesPackage = mkOption {
-    #type = package;
-    #default = pkgs.nixFlakes;
-    #};
-    nixRebuildPackage = mkOption {
-      type = package;
-      default = pkgs.nixos-rebuild;
-    };
   };
 
   config =
     let
       cfg = config.services.nixos-auto-update;
       gitPath = "${cfg.gitPackage}/bin/git";
-      #nixFlakesPath = "${cfg.nixFlakesPackage}/bin/nix";
-      nixRebuildPath = "${cfg.nixRebuildPackage}/bin/nixos-rebuild";
       mkStartScript = name: pkgs.writeShellScript "${name}.sh" ''
         set -euo pipefail
         PATH=${makeBinPath (with pkgs; [ git ])}
         export NIX_PATH="nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos:nixos-config=/etc/nixos/configuration.nix"
         cd /etc/nixos/
         ${gitPath} pull origin master
-        /run/current-system/sw/bin/nixos-rebuild switch --flake '/etc/nixos/#nixtst' --impure
-        #${nixRebuildPath} switch --flake '/etc/nixos/#nixtst' --impure
+        ${config.system.build.nixos-rebuild}/bin/nixos-rebuild switch --flake '/etc/nixos/#nixtst' --impure
       '';
     in
     mkIf cfg.enable (
@@ -44,8 +33,8 @@ with lib;
           ExecStart = "${mkStartScript name}";
         };
         timerConfig = {
-          OnBootSec = "1m"; # first run 5min after boot up
-          OnUnitActiveSec = "1w"; # run weekly
+          OnBootSec = "5m"; # first run 5min after boot up
+          #OnUnitActiveSec = "1w"; # run weekly
         };
       }
     );
